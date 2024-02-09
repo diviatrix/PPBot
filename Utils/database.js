0c0afcd65d30c7f22c414e566cb710b3fdca8048
Oleg Chura <oleg.chura@glera-games.com>
2024-02-09 22:56:04 +0700

    Create database.js

---
+++ Utils/database.js
@@ -0,0 +1,25 @@
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+class Database {
+  constructor(filePath) {
+    this.filePath = filePath;
+    this.db = null;
+  }
+
+  connect() {
+    const adapter = new FileSync(this.filePath);
+    this.db = low(adapter);
+    // Add any necessary configurations or defaults here
+	this.db.defaults({ users: [] }).write();
+  }
+
+  getDb() {
+    if (!this.db) {
+      throw new Error('Database not connected. Call connect() first.');
+    }
+    return this.db;
+  }
+}
+
+module.exports = Database;
